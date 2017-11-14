7e40934d645bf67d26a10a6e932518d76bff3f8c
Geno <geno+dev@fireorbit.de>
2017-11-14 13:46:20 +0100

    Update config.go

--- runtime/config.go
+++ runtime/config.go
@@ -5,21 +5,9 @@ import (
 )
 
 type Config struct {
-	Folder     string                `toml:"folder"`
 	RunEvery   yanicRuntime.Duration `toml:"run_every"`
 	Meshviewer struct {
-		ConfigTemplate string      `toml:"config_template"`
-		ConfigOutput   string      `toml:"config_output"`
-		DataPathURL    string      `toml:"data_path_url"`
-		DataPath       []*DataPath `toml:"dataPath"`
-		SiteNames      []struct {
-			Site string `toml:"site" json:"site"`
-			Name string `toml:"name" json:"name"`
-		} `toml:"siteNames"`
+		Output     string    `toml:"output"`
+		DataPaths  []string  `toml:"dataPaths"`
 	} `toml:"meshviewer"`
-}
-
-type DataPath struct {
-	URL      string `toml:"url"`
-	Filename string `toml:"filename"`
 }
